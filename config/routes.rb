cba1d9517ef9088feaee3a09a5fac34ace90eb35
Polo <paul.poitrenaud@gmail.com>
2019-11-05 11:26:05 +0100

    v2 devise ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'events#index'
+  get 'static_pages/index'
+  get 'static_pages/secret'
+  devise_for :users
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
